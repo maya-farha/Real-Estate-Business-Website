e766aaf716136eeb51123bf2611f5936a0e0ea74
maya-farha <maiaafarha@gmail.com>
2025-08-05 12:17:43 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/AboutUs/About";
-import Prperty from "./pages/property/Property";
-import Service from "./pages/services/Service";
-import Contact from "./pages/contact/Contact";
+import Prperty from "./pages/Property/Property";
+import Service from "./pages/Services/Service";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/property" element={<Prperty />}></Route>
-        <Route path="/about" element={<Service />}></Route>
-        <Route path="/about" element={<Contact />}></Route>
+        <Route path="/service" element={<Service />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
   );
