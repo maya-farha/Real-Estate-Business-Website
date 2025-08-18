11bcb09f778b6178011619fe5b86ccc144734add
majd-shbani <majdshbani9@gmail.com>
2025-08-19 02:17:33 +0300

    slider movement

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import "./Home.css";
 import ServiceSection from "../../components/ServiceSection/ServiceSection";
 
 import HeroHome from "../../components/HeroHome/HeroHome";
+import ClientSays from "../../components/ClientSays/ClientSays";
+import AskedQuestions from "../../components/AskedQuestions/AskedQuestions";
+
 
 function Home() {
   return (
@@ -10,6 +13,8 @@ function Home() {
       <HeroHome></HeroHome>
       <div className="MF-serviceSection">
         <ServiceSection></ServiceSection>
+        <ClientSays/>
+        <AskedQuestions/>
       </div>
     </div>
   );
